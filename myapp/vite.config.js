004f2d47f8401cc05ea16fe379883a06152b6e1f
rushikeshwayal <rushikeshwayal6@gmail.com>
2025-10-20 21:59:52 +0530

    updating vercel.json and vite.config.js

--- myapp/vite.config.js
+++ myapp/vite.config.js
@@ -1,17 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   build: {
     outDir: "dist",
     sourcemap: true,
   },
-  base: "/", // important for Vercel
+  base: "/",
+  // This proxy is only for the local development server.
+  // It will not be used in the production build on Vercel.
+  // Vercel uses the `vercel.json` file for production routing.
   server: {
     proxy: {
       "/api": {
+        // Your backend server running locally
         target: "http://localhost:8000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
